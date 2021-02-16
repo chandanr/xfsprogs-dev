268bb9dd0aaa729c900981fd39c920d67af49292
Chandan Babu R <chandanrlinux@gmail.com>
2021-02-16 19:11:00 +0530

    xfsprogs: Rename inode's extent counter fields based on their width
    This commit renames extent counter fields in "struct xfs_dinode" and "struct
    xfs_log_dinode" based on the width of the fields. As of this commit, the
    32-bit field will be used to count data fork extents and the 16-bit field will
    be used to count attr fork extents.
    
    This change is done to enable a future commit to introduce a new 64-bit extent
    counter field.
    
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- repair/bmap_repair.c
+++ repair/bmap_repair.c
@@ -536,7 +536,7 @@ rebuild_bmap(
 		if (nextents == 0)
 			return 0;
 		(*dinop)->di_format = XFS_DINODE_FMT_EXTENTS;
-		(*dinop)->di_nextents = 0;
+		(*dinop)->di_nextents32 = 0;
 		libxfs_dinode_calc_crc(mp, *dinop);
 		*dirty = 1;
 		break;
@@ -547,7 +547,7 @@ rebuild_bmap(
 		if (nextents == 0)
 			return 0;
 		(*dinop)->di_aformat = XFS_DINODE_FMT_EXTENTS;
-		(*dinop)->di_anextents = 0;
+		(*dinop)->di_nextents16 = 0;
 		libxfs_dinode_calc_crc(mp, *dinop);
 		*dirty = 1;
 		break;
