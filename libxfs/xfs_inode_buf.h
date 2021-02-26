5f7f296b92eb2ebf05f0d03f2a2b7b297b030232
Chandan Babu R <chandanrlinux@gmail.com>
2021-02-26 17:18:35 +0530

    xfsprogs: xfs_dfork_nextents: Return extent count via an out argument
    This commit changes xfs_dfork_nextents() to return an error code. The extent
    count itself is now returned through an out argument. This facility will be
    used by a future commit to indicate an inconsistent ondisk extent count.
    
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- libxfs/xfs_inode_buf.h
+++ libxfs/xfs_inode_buf.h
@@ -36,8 +36,8 @@ xfs_failaddr_t xfs_inode_validate_extsize(struct xfs_mount *mp,
 xfs_failaddr_t xfs_inode_validate_cowextsize(struct xfs_mount *mp,
 		uint32_t cowextsize, uint16_t mode, uint16_t flags,
 		uint64_t flags2);
-xfs_extnum_t xfs_dfork_nextents(struct xfs_mount *mp, struct xfs_dinode *dip,
-		int whichfork);
+int xfs_dfork_nextents(struct xfs_mount *mp, struct xfs_dinode *dip,
+		int whichfork, xfs_extnum_t *nextents);
 
 static inline uint64_t xfs_inode_encode_bigtime(struct timespec64 tv)
 {
