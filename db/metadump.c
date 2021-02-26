84b54eb68311ba9c35a3581d8d55617222dac6e3
Chandan Babu R <chandanrlinux@gmail.com>
2021-02-26 11:02:31 +0530

    xfsprogs: Introduce xfs_dfork_nextents() helper
    This commit replaces the macro XFS_DFORK_NEXTENTS() with the helper function
    xfs_dfork_nextents(). As of this commit, xfs_dfork_nextents() returns the same
    value as XFS_DFORK_NEXTENTS(). A future commit which extends inode's extent
    counter fields will add more logic to this helper.
    
    This commit also replaces direct accesses to xfs_dinode->di_[a]nextents
    with calls to xfs_dfork_nextents().
    
    No functional changes have been made.
    
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- db/metadump.c
+++ db/metadump.c
@@ -2314,7 +2314,7 @@ process_exinode(
 
 	whichfork = (itype == TYP_ATTR) ? XFS_ATTR_FORK : XFS_DATA_FORK;
 
-	nex = XFS_DFORK_NEXTENTS(dip, whichfork);
+	nex = xfs_dfork_nextents(dip, whichfork);
 	used = nex * sizeof(xfs_bmbt_rec_t);
 	if (nex < 0 || used > XFS_DFORK_SIZE(dip, mp, whichfork)) {
 		if (show_warnings)
@@ -2369,7 +2369,7 @@ static int
 process_dev_inode(
 	xfs_dinode_t		*dip)
 {
-	if (XFS_DFORK_NEXTENTS(dip, XFS_DATA_FORK)) {
+	if (xfs_dfork_nextents(dip, XFS_DATA_FORK)) {
 		if (show_warnings)
 			print_warning("inode %llu has unexpected extents",
 				      (unsigned long long)cur_ino);
