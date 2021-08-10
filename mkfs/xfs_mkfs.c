6ee3a54ac6888aabb02e6f183a48637277a168c7
Chandan Babu R <chandan.babu@oracle.com>
2021-08-10 16:01:08 +0530

    xfsprogs: Define max extent length based on on-disk format definition
    The maximum extent length depends on maximum block count that can be stored in
    a BMBT record. Hence this commit defines MAXEXTLEN based on
    BMBT_BLOCKCOUNT_BITLEN.
    
    While at it, the commit also renames MAXEXTLEN to XFS_MAX_BMBT_EXTLEN.
    
    Suggested-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>

--- mkfs/xfs_mkfs.c
+++ mkfs/xfs_mkfs.c
@@ -2398,7 +2398,7 @@ validate_extsize_hint(
 		fprintf(stderr,
 _("illegal extent size hint %lld, must be less than %u.\n"),
 				(long long)cli->fsx.fsx_extsize,
-				min(MAXEXTLEN, mp->m_sb.sb_agblocks / 2));
+				min(XFS_MAX_BMBT_EXTLEN, mp->m_sb.sb_agblocks / 2));
 		usage();
 	}
 
@@ -2421,7 +2421,7 @@ _("illegal extent size hint %lld, must be less than %u.\n"),
 		fprintf(stderr,
 _("illegal extent size hint %lld, must be less than %u and a multiple of %u.\n"),
 				(long long)cli->fsx.fsx_extsize,
-				min(MAXEXTLEN, mp->m_sb.sb_agblocks / 2),
+				min(XFS_MAX_BMBT_EXTLEN, mp->m_sb.sb_agblocks / 2),
 				mp->m_sb.sb_rextsize);
 		usage();
 	}
@@ -2450,7 +2450,7 @@ validate_cowextsize_hint(
 		fprintf(stderr,
 _("illegal CoW extent size hint %lld, must be less than %u.\n"),
 				(long long)cli->fsx.fsx_cowextsize,
-				min(MAXEXTLEN, mp->m_sb.sb_agblocks / 2));
+				min(XFS_MAX_BMBT_EXTLEN, mp->m_sb.sb_agblocks / 2));
 		usage();
 	}
 }
