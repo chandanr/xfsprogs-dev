34dadf94474905f688c678cbb783621e41396eed
Chandan Babu R <chandanrlinux@gmail.com>
2021-08-11 10:14:18 +0530

    xfsprogs: Extend per-inode extent counter widths
    This commit adds a new 64-bit per-inode data extent counter. However the
    maximum number of extents that a data fork can hold is limited to 2^48
    extents. This feature is available only when
    XFS_SB_FEAT_INCOMPAT_EXTCOUNT_64BIT feature bit is enabled on the
    filesystem. Also, enabling this feature bit causes attr fork extent counter to
    use the 32-bit extent counter that was previously used to hold the data fork
    extent counter. This implies that the attr fork can now occupy a maximum of
    2^32 extents.
    
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- libxfs/xfs_bmap.c
+++ libxfs/xfs_bmap.c
@@ -48,18 +48,16 @@ xfs_bmap_compute_maxlevels(
 	int		whichfork)	/* data or attr fork */
 {
 	xfs_extnum_t	maxleafents;	/* max leaf entries possible */
+	xfs_rfsblock_t	maxblocks;	/* max blocks at this level */
 	int		level;		/* btree level */
-	uint		maxblocks;	/* max blocks at this level */
 	int		maxrootrecs;	/* max records in root block */
 	int		minleafrecs;	/* min records in leaf block */
 	int		minnoderecs;	/* min records in node block */
 	int		sz;		/* root block size */
 
 	/*
-	 * The maximum number of extents in a file, hence the maximum number of
-	 * leaf entries, is controlled by the size of the on-disk extent count,
-	 * either a signed 32-bit number for the data fork, or a signed 16-bit
-	 * number for the attr fork.
+	 * The maximum number of extents in a fork, hence the maximum number of
+	 * leaf entries, is controlled by the size of the on-disk extent count.
 	 *
 	 * Note that we can no longer assume that if we are in ATTR1 that the
 	 * fork offset of all the inodes will be
