44831e33463e819abb4f8b6f78645b08b55f973c
Chandan Babu R <chandan.babu@oracle.com>
2021-11-17 07:29:41 +0000

    xfsprogs: Introduce macros to represent new maximum extent counts for data/attr forks
    This commit defines new macros to represent maximum extent counts allowed by
    filesystems which have support for large per-inode extent counters.
    
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>

--- libxfs/xfs_inode_buf.c
+++ libxfs/xfs_inode_buf.c
@@ -358,7 +358,8 @@ xfs_dinode_verify_fork(
 			return __this_address;
 		break;
 	case XFS_DINODE_FMT_BTREE:
-		max_extents = xfs_iext_max_nextents(whichfork);
+		max_extents = xfs_iext_max_nextents(xfs_dinode_has_nrext64(dip),
+						whichfork);
 		if (di_nextents > max_extents)
 			return __this_address;
 		break;
