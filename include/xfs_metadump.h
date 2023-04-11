a7030242ad9c9c165155ba8efe274e4a2b52abaa
Chandan Babu R <chandan.babu@oracle.com>
2023-04-11 15:12:11 +0530

    include/xfs_metadump.h: Define XFS_MD_MAGIC_V2 and incompat flags
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>

--- include/xfs_metadump.h
+++ include/xfs_metadump.h
@@ -8,6 +8,7 @@
 #define _XFS_METADUMP_H_
 
 #define	XFS_MD_MAGIC		0x5846534d	/* 'XFSM' */
+#define	XFS_MD_MAGIC_V2		0x584D4432	/* 'XMD2' */
 
 typedef struct xfs_metablock {
 	__be32		mb_magic;
@@ -22,5 +23,9 @@ typedef struct xfs_metablock {
 #define XFS_METADUMP_OBFUSCATED	(1 << 1)
 #define XFS_METADUMP_FULLBLOCKS	(1 << 2)
 #define XFS_METADUMP_DIRTYLOG	(1 << 3)
+
+#define XFS_MD2_INCOMPAT_OBFUSCATED	(1 << 0)
+#define XFS_MD2_INCOMPAT_FULLBLOCKS	(1 << 1)
+#define XFS_MD2_INCOMPAT_DIRTYLOG	(1 << 2)
 
 #endif /* _XFS_METADUMP_H_ */
