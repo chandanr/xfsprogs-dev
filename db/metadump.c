17a02703f2ba9a173a6bcbcf477dfdb71a903026
Chandan Babu R <chandan.babu@oracle.com>
2023-04-11 15:28:56 +0530

    metadump: Postpone invocation of init_metadump()
    The metadump v2 initialization function (introduced in a later commit) writes
    the header structure into the metadump file. This will require the program to
    open the metadump file before the initialization function has been invoked.
    
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>

--- db/metadump.c
+++ db/metadump.c
@@ -3124,10 +3124,6 @@ metadump_f(
 		pop_cur();
 	}
 
-	ret = init_metadump();
-	if (ret)
-		return 0;
-
 	start_iocur_sp = iocur_sp;
 
 	if (strcmp(argv[optind], "-") == 0) {
@@ -3172,6 +3168,10 @@ metadump_f(
 		}
 	}
 
+	ret = init_metadump();
+	if (ret)
+		goto out;
+
 	exitcode = 0;
 
 	for (agno = 0; agno < mp->m_sb.sb_agcount; agno++) {
@@ -3209,8 +3209,9 @@ metadump_f(
 	/* cleanup iocur stack */
 	while (iocur_sp > start_iocur_sp)
 		pop_cur();
-out:
+
 	release_metadump();
 
+out:
 	return 0;
 }
