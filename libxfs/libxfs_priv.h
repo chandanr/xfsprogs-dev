12e1b18ddf4fec08512696ec71bb83e652fb11c6
Chandan Babu R <chandanbabu@kernel.org>
2024-10-18 19:22:41 +0530

    Fix compilation errors
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- libxfs/libxfs_priv.h
+++ libxfs/libxfs_priv.h
@@ -451,17 +451,6 @@ int xfs_trans_roll(struct xfs_trans **);
 void xfs_trans_add_item(struct xfs_trans *, struct xfs_log_item *);
 void xfs_trans_del_item(struct xfs_log_item *);
 
-/* xfs_inode_item.c */
-void xfs_inode_item_init(struct xfs_inode *, struct xfs_mount *);
-
-/* xfs_buf_item.c */
-void xfs_buf_item_init(struct xfs_buf *, struct xfs_mount *);
-void xfs_buf_item_log(struct xfs_buf_log_item *, uint, uint);
-
-/* xfs_trans_buf.c */
-struct xfs_buf *xfs_trans_buf_item_match(struct xfs_trans *,
-			struct xfs_buftarg *, struct xfs_buf_map *, int);
-
 /* local source files */
 #define xfs_mod_fdblocks(mp, delta, rsvd) \
 	libxfs_mod_incore_sb(mp, XFS_TRANS_SB_FDBLOCKS, delta, rsvd)
