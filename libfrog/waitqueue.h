bc6c225dcadb6d01e546127f6b66c78c6620fdb4
Chandan Babu R <chandanbabu@kernel.org>
2024-10-28 13:16:42 +0530

    Add dummy waitqueue functionality
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

---
+++ libfrog/waitqueue.h
@@ -0,0 +1,22 @@
+#ifndef _WAITQUEUE_H
+#define _WAITQUEUE_H
+
+#define TASK_KILLABLE 0
+
+typedef struct wait_queue_head {
+	;
+} wait_queue_head_t;
+
+struct wait_queue_entry {
+	;
+};
+
+
+#define wake_up(...) ((void)0)
+#define wake_up_all(...) ((void)0)
+#define DEFINE_WAIT(name) struct wait_queue_entry name = {};
+#define prepare_to_wait(...) ((void)0)
+#define waitqueue_active(a) true
+#define finish_wait(...) ((void)0)
+
+#endif	/* _WAITQUEUE_H */
