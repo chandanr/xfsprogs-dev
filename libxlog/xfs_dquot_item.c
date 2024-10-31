fac100442635b025406c65a8489e08f631d84de0
Chandan Babu R <chandanbabu@kernel.org>
2024-10-31 18:05:53 +0530

    Fix compilation errors
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- libxlog/xfs_dquot_item.c
+++ libxlog/xfs_dquot_item.c
@@ -3,7 +3,11 @@
  * Copyright (c) 2000-2003 Silicon Graphics, Inc.
  * All Rights Reserved.
  */
+
+#include "libxfs.h"
 #include "libxlog_priv.h"
+#include "libxlog.h"
+
 #include "xfs.h"
 #include "xfs_fs.h"
 #include "xfs_shared.h"
