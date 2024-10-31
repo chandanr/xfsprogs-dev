fbd7d38218fea60b2cb9a30d9a2f4638b4abde01
Chandan Babu R <chandanbabu@kernel.org>
2024-10-31 18:05:16 +0530

    Add a dummy definition for wait_event()
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- libfrog/waitqueue.h
+++ libfrog/waitqueue.h
@@ -16,6 +16,7 @@ struct wait_queue_entry {
 #define wake_up(...) ((void)0)
 #define wake_up_all(...) ((void)0)
 #define wake_up_process(...) ((void)0)
+#define wait_event(...) ((void)0)
 
 #define DEFINE_WAIT(name) \
 	do {\
