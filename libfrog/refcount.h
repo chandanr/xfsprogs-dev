d44ce7b18e26eefa32edb093d05887e4ab4ef8d3
Chandan Babu R <chandanbabu@kernel.org>
2025-09-10 09:20:27 +0530

    Disable refcount APIs
    We don't support attribute logging at this point.
    
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- libfrog/refcount.h
+++ libfrog/refcount.h
@@ -1,13 +1,8 @@
 #ifndef _REFCOUNT_H
 #define _REFCOUNT_H
 
-typedef struct refcount {
-	/* chandan: should this be atomic_t as in the kernel? */
-	u32 refs;
-} refcount_t;
-
-#define refcount_inc_not_zero(refcount) (1)
-#define refcount_dec_and_test(refcount) (1)
-#define refcount_set(refcount, nr)
+#define refcount_inc_not_zero(refcount) ASSERT(0)
+#define refcount_dec_and_test(refcount) ASSERT(0)
+#define refcount_set(refcount, nr)	ASSERT(0)
 
 #endif	/* _REFCOUNT_H */
