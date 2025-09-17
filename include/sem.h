8253b4921ea7b6d669bf9f808f134e2d21e28d9a
Chandan Babu R <chandanbabu@kernel.org>
2025-09-17 10:53:33 +0530

    Use pthread mutex for implementing semaphores
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

---
+++ include/sem.h
@@ -0,0 +1,41 @@
+#ifndef __SEM_H__
+#define __SEM_H__
+
+#include <pthread.h>
+
+struct semaphore {
+	pthread_mutex_t mutex;
+};
+
+static inline void
+sema_init(
+	struct semaphore	*sem,
+	int			val)
+{
+	int error;
+
+	error = pthread_mutex_init(&sem->mutex, NULL);
+	ASSERT(error == 0);
+}
+
+static inline void
+down(
+	struct semaphore	*sem)
+{
+	int			error;
+
+	error = pthread_mutex_lock(&sem->mutex);
+	ASSERT(error == 0);
+}
+
+static inline void
+up(
+	struct semaphore	*sem)
+{
+	int			error;
+
+	error = pthread_mutex_unlock(&sem->mutex);
+	ASSERT(error == 0);
+}
+
+#endif	/* __SEM_H__ */
