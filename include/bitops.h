920cf83b3e069d436a3688158d0ccacc56894908
Chandan Babu R <chandanbabu@kernel.org>
2025-10-07 13:32:01 +0530

    bitops.h: Add some more definitions
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- include/bitops.h
+++ include/bitops.h
@@ -2,6 +2,59 @@
 #ifndef __BITOPS_H__
 #define __BITOPS_H__
 
+#define BITS_TO_LONGS(nr)       DIV_ROUND_UP(nr, NBBY * sizeof(long))
+#define DECLARE_BITMAP(name,bits) \
+	unsigned long name[BITS_TO_LONGS(bits)]
+#define BITMAP_FIRST_WORD_MASK(start) (~0UL << ((start) & (BITS_PER_LONG - 1)))
+
+/*
+ * This is a common helper function for find_next_bit and
+ * find_next_zero_bit.  The difference is the "invert" argument, which
+ * is XORed with each fetched word before searching it for one bits.
+ */
+static inline unsigned long
+_find_next_bit(const unsigned long *addr, unsigned long nbits,
+		unsigned long start, unsigned long invert)
+{
+	unsigned long tmp;
+
+	if (!nbits || start >= nbits)
+		return nbits;
+
+	tmp = addr[start / BITS_PER_LONG] ^ invert;
+
+	/* Handle 1st word. */
+	tmp &= BITMAP_FIRST_WORD_MASK(start);
+	start = round_down(start, BITS_PER_LONG);
+
+	while (!tmp) {
+		start += BITS_PER_LONG;
+		if (start >= nbits)
+			return nbits;
+
+		tmp = addr[start / BITS_PER_LONG] ^ invert;
+	}
+
+	return min(start + ffs(tmp), nbits);
+}
+
+/*
+ * Find the next set bit in a memory region.
+ */
+static inline unsigned long
+find_next_bit(const unsigned long *addr, unsigned long size,
+		unsigned long offset)
+{
+	return _find_next_bit(addr, size, offset, 0UL);
+}
+static inline unsigned long
+find_next_zero_bit(const unsigned long *addr, unsigned long size,
+		 unsigned long offset)
+{
+	return _find_next_bit(addr, size, offset, ~0UL);
+}
+#define find_first_zero_bit(addr, size) find_next_zero_bit((addr), (size), 0)
+
 /*
  * fls: find last bit set.
  */
--- include/libxfs.h
+++ include/libxfs.h
@@ -339,45 +339,6 @@ bool libxfs_verify_rtbno(struct xfs_mount *mp, xfs_rtblock_t rtbno);
 #include "xfs_attr.h"
 #include "topology.h"
 
-/*
- * Handling for kernel bitmap types.
- */
-#define BITS_TO_LONGS(nr)       DIV_ROUND_UP(nr, NBBY * sizeof(long))
-#define DECLARE_BITMAP(name,bits) \
-	unsigned long name[BITS_TO_LONGS(bits)]
-#define BITMAP_FIRST_WORD_MASK(start) (~0UL << ((start) & (BITS_PER_LONG - 1)))
-
-/*
- * This is a common helper function for find_next_bit and
- * find_next_zero_bit.  The difference is the "invert" argument, which
- * is XORed with each fetched word before searching it for one bits.
- */
-static inline unsigned long
-_find_next_bit(const unsigned long *addr, unsigned long nbits,
-		unsigned long start, unsigned long invert)
-{
-	unsigned long tmp;
-
-	if (!nbits || start >= nbits)
-		return nbits;
-
-	tmp = addr[start / BITS_PER_LONG] ^ invert;
-
-	/* Handle 1st word. */
-	tmp &= BITMAP_FIRST_WORD_MASK(start);
-	start = round_down(start, BITS_PER_LONG);
-
-	while (!tmp) {
-		start += BITS_PER_LONG;
-		if (start >= nbits)
-			return nbits;
-
-		tmp = addr[start / BITS_PER_LONG] ^ invert;
-	}
-
-	return min(start + ffs(tmp), nbits);
-}
-
 static inline __attribute__((const))
 int is_power_of_2(unsigned long n)
 {
@@ -408,23 +369,6 @@ roundup_pow_of_two(uint v)
 	ASSERT(0);
 	return 0;
 }
-
-/*
- * Find the next set bit in a memory region.
- */
-static inline unsigned long
-find_next_bit(const unsigned long *addr, unsigned long size,
-		unsigned long offset)
-{
-	return _find_next_bit(addr, size, offset, 0UL);
-}
-static inline unsigned long
-find_next_zero_bit(const unsigned long *addr, unsigned long size,
-		 unsigned long offset)
-{
-	return _find_next_bit(addr, size, offset, ~0UL);
-}
-#define find_first_zero_bit(addr, size) find_next_zero_bit((addr), (size), 0)
 
 /* local source files */
 #define xfs_mod_fdblocks(mp, delta, rsvd) \
