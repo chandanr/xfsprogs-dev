6382fa8e8249a50445f503641d142f921d77dd02
Chandan Babu R <chandanbabu@kernel.org>
2025-10-10 15:57:11 +0530

    include/libxlog.h: Remove unnecessary macros
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- include/libxlog.h
+++ include/libxlog.h
@@ -28,15 +28,6 @@ struct xfs_kobj {
 	;
 };
 
-#define smp_rmb()
-
-/* TODO: chandan: implement waitqueues */
-#define wait_var_event(var, cond)
-#define DECLARE_WAITQUEUE(wait, current)
-#define add_wait_queue_exclusive(wq, waitp)
-#define remove_wait_queue(wq, waitp)
-#define __set_current_state(state)
-#define schedule()
 
 /*
  * #include "xfs_attr_item.h"
