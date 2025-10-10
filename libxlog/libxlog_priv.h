76a44acb4b19eb30494dcd76d1f5fea6d8a6cf07
Chandan Babu R <chandanbabu@kernel.org>
2025-10-10 15:43:50 +0530

    libxlog/libxlog_priv.h: Remove unnecessary include statements
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- libxlog/libxlog_priv.h
+++ libxlog/libxlog_priv.h
@@ -1,10 +1,6 @@
 #ifndef _LIBXLOG_PRIV_H
 #define _LIBXLOG_PRIV_H
 
-#include "libxlog_api_defs.h"
-#include "platform_defs.h"
-#include "xfs.h"
-
 #define STATIC				static
 
 #define xfs_alert_tag(...)
