d72415d34116f8f74bb8b52660ea2164d7e1d695
Chandan Babu R <chandanbabu@kernel.org>
2025-10-10 16:16:16 +0530

    Define struct refcount_struct
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- libfrog/refcount.h
+++ libfrog/refcount.h
@@ -1,6 +1,10 @@
 #ifndef _REFCOUNT_H
 #define _REFCOUNT_H
 
+typedef struct refcount_struct {
+	atomic_t refs;
+} refcount_t;
+
 #define refcount_inc_not_zero(refcount) ASSERT(0)
 #define refcount_dec_and_test(refcount) ASSERT(0)
 #define refcount_set(refcount, nr)	ASSERT(0)
