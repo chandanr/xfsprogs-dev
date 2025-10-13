b9f575251c8864d3a69450de5828c6dbf7cf000b
Chandan Babu R <chandanbabu@kernel.org>
2025-10-13 11:46:46 +0530

    xfs_inode_item_recover.c
    Signed-off-by: Chandan Babu R <chandanbabu@kernel.org>

--- libxlog/xfs_inode_item_recover.c
+++ libxlog/xfs_inode_item_recover.c
@@ -3,9 +3,39 @@
  * Copyright (c) 2000-2006 Silicon Graphics, Inc.
  * All Rights Reserved.
  */
-#include "libxfs.h"
+#include "generic_headers.h"
+
+#include "platform_defs.h"
+
+#include "kernel_types.h"
+#include "kernel_misc_stage1.h"
+
+#include "xfsprogs_helpers.h"
+
+/* Header files from libfrog/ */
+#include "libfrog/refcount.h"
+#include "libfrog/radix-tree.h"
+#include "libfrog/rbtree.h"
+#include "libfrog/crc32c.h"
+#include "libfrog/bio.h"
+#include "libfrog/pseudo_percpu.h"
+#include "libfrog/schedule.h"
+#include "libfrog/waitqueue.h"
+#include "libfrog/workqueue.h"
+#include "libfrog/delayed-work.h"
+
+/* chandan: xfs/xfs_types.h declares xfs_verify_*() */
+#include "libxfs_api_defs.h"
+#include "libxlog_api_defs.h"
+
+/* XFS header files from xfsprogs/include/ */
+#include "xfs.h"
+#include "xfs_arch.h"
+
+#include "kernel_misc_stage2.h"
+
+/* Header files from libxlog/ */
 #include "libxlog_priv.h"
-#include "libxlog.h"
 
 #include "xfs.h"
 #include "xfs_fs.h"
